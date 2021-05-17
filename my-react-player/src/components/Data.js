428d38dce0d4bd14793fb25d173e07cc05e87a95
Matt Harrison <MattHarrison92@gmail.com>
2021-05-17 15:31:00 -0400

    fixed link

--- my-react-player/src/components/Data.js
+++ my-react-player/src/components/Data.js
@@ -1,5 +1,4 @@
 import { v4 as uuidv4 } from "uuid";
-
 function chillHop() {
     return [
         {
@@ -7,7 +6,7 @@ function chillHop() {
             cover:
                 "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
             artist: "Aso, Middle School, Aviino",
-            audio: "",
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
             color: ["#205950", "#2ab3bf"],
             id: uuidv4(),
             active: true,
